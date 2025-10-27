cac177774f15d420345b7ece3ad47f7e70f2499b
tyler.blumenstein <tyler.blumenstein@NB-043.wyler.zh>
2025-10-27 11:03:08 +0100

    name change

--- RedLEVEL/Core/Src/main.c
+++ RedLEVEL/Core/Src/main.c
@@ -77,7 +77,7 @@ static void MX_I2C1_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-//test change 6
+//test change 0
 
 void MPU6500_Init(void)
 {
